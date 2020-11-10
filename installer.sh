d3809745af4007ff8aa193f512f22c2544c65f5d
Rubén Laplaza <30357710+rlaplaza@users.noreply.github.com>
2020-11-10 15:49:39 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git submodule update --init
-meson setup build --prefix=$PWD --libdir=xtb
+DFLAGS="-L${MKLROOT}/lib/intel64" meson setup build --prefix=$PWD --libdir=xtb
 ninja -C build install
 pip install -e . --user 
